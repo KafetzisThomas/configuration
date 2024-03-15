eef52d3f123592bd0e3c82a91e2ce16c04fc13be
KafetzisThomas <tomkafetzis06@gmail.com>
2024-03-15 23:57:59 +0200

    Add project details

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-############################
+
+# Project Title: configuration (https://github.com/KafetzisThomas/configuration)
+# Author / Project Owner: KafetzisThomas (https://github.com/KafetzisThomas)
+# License: MIT
+
+########################################
 # This script:
 #   installs Linux [Ubuntu] Software
 #   installs Apt & Snap Packages (Apps)
 #   sets up VS Code
-############################
+########################################
 
 # Run the ubuntu script
 ./ubuntu.sh
