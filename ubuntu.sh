b218bf42d4ac6e3ec7d846a153918c8cfc41665e
KafetzisThomas <tomkafetzis06@gmail.com>
2024-03-15 23:11:00 +0200

    Enable auto-hide dock utility

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,5 +5,9 @@ IMAGE_PATH="${HOME}/configuration/settings/Mirror_by_Uday_Nakade.jpg"
 
 # Set the desktop wallpaper using gsettings
 gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_PATH"
-
 echo "Desktop wallpaper set successfully."
+
+# Enable auto-hide dock utility
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+echo "Auto-hide dock utility enabled."
