f8429c8cd50c1a0cc3833eb2404d52caa261fd71
KafetzisThomas <tomkafetzis06@gmail.com>
2024-03-17 20:47:13 +0200

    Remove operations that dont work properly
    Maybe gsettings is not so good on configuring the dock after all...

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,37 +1,12 @@
 #!/bin/bash
 
+# Enable dark mode
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+echo "Dark mode enabled."
+
 # Get the absolute path to the image
 IMAGE_PATH="${HOME}/configuration/settings/Mirror_by_Uday_Nakade.jpg"
 
 # Set the desktop wallpaper using gsettings
-gsettings set org.gnome.desktop.background picture-uri "file://$IMAGE_PATH"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$IMAGE_PATH"
 echo "Desktop wallpaper set successfully."
-
-# Enable auto-hide dock utility
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height true
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-echo "Auto-hide dock utility enabled."
-
-# Change the dock position to the bottom
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
-echo "Dock position moved to the bottom."
-
-# Set the icon size of apps in the dock to 48
-gsettings set org.gnome.shell.extensions.dash-to-dock icon-size 48
-echo "Icon size of apps in the dock changed to 48."
-
-# Disable the appearance of the personal folder icon on the desktop
-gsettings set org.gnome.shell.extensions.ding show-home false
-echo "Personal folder icon on the desktop disabled."
-
-# Enable dark mode
-gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
-echo "Dark mode enabled."
-
-# Include volumes and devices, network volumes, and trash to the dock
-gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts true
-
-# Exclude unmounted volumes from the dock
-gsettings set org.gnome.shell.extensions.dash-to-dock show-unmounted-volumes false
-
-echo "Dock behavior configured."
