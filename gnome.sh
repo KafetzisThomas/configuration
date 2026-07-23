4ce1333b5fdeb3978831806b0fbc42d49c4ae717
KafetzisThomas <tomkafetzis06@gmail.com>
2026-07-23 10:13:30 +0300

    Add blur-my-shell to gnome

--- gnome.sh
+++ gnome.sh
@@ -7,12 +7,16 @@ pipx install gnome-extensions-cli
 pipx ensurepath
 
 
-echo "Installing Battery Health Charging..."
+echo "Installing Battery-Health-Charging..."
 $HOME/.local/bin/gext install Battery-Health-Charging@maniacx.github.com
 
 
 echo "Installing Vitals..."
 $HOME/.local/bin/gext install Vitals@CoreCoding.com
+
+
+echo "Installing blur-my-shell..."
+$HOME/.local/bin/gext install blur-my-shell@aunetx
 
 
 echo "==============================================="
