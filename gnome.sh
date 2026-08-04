ff365229161f5960b3fb59243e65beb9da17d9df
KafetzisThomas <tomkafetzis06@gmail.com>
2026-08-04 12:18:52 +0300

    Configure gnome font size

--- gnome.sh
+++ gnome.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Setting up dark mode and walpaper..."
+WALLPAPER_PATH="$HOME/Downloads/configuration/assets/wallpaper.jpg"
 
-WALLPAPER_PATH="$HOME/Downloads/configuration/assets/walpaper.jpg"
+echo "Setting up font size, dark mode and wallpaper..."
 
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.background picture-uri-dark "file://${WALLPAPER_PATH}"
 gsettings set org.gnome.desktop.background picture-uri "file://${WALLPAPER_PATH}"
