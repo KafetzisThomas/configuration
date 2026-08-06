72d502589206ee4887706a3ce0befe9e3df583b0
KafetzisThomas <tomkafetzis06@gmail.com>
2026-08-06 09:27:12 +0300

    Set git creds from terminal + update projects dir path

--- git.sh
+++ git.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -e
 
+PROJECTS_DIR="$HOME/Projects"
+mkdir -p "$PROJECTS_DIR"
+
 echo "Configuring Git..."
-git config --global user.name "KafetzisThomas"
-git config --global user.email "tomkafetzis06@gmail.com"
+
+echo "Enter your Git username:"
+read GIT_USERNAME
+
+echo "Enter your Git email:"
+read GIT_EMAIL
+
+git config --global user.name "$GIT_USERNAME"
+git config --global user.email "$GIT_EMAIL"
 git config --global init.defaultBranch main
 
-echo "Setting up GitHub CLI & repositories..."
-
-PROJECTS_DIR="$HOME/Documents/Projects"
-mkdir -p "$PROJECTS_DIR"
+echo "Setting up GitHub CLI & cloning repos to $PROJECTS_DIR..."
 
 gh auth login --web
 
