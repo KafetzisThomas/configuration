e50ed0d40424a0573e49c69eb608b47cb5135182
KafetzisThomas <tomkafetzis06@gmail.com>
2026-08-06 09:18:16 +0300

    Install gnome extensions manually with extensions manager

--- gnome.sh
+++ gnome.sh
@@ -10,30 +10,20 @@ gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.background picture-uri-dark "file://${WALLPAPER_PATH}"
 gsettings set org.gnome.desktop.background picture-uri "file://${WALLPAPER_PATH}"
 
-echo "Installing GNOME extensions..."
-sudo dnf install -y pipx
-pipx install gnome-extensions-cli
-pipx ensurepath
+echo "Open Extension Manager:"
 
-export PATH="$HOME/.local/bin:$PATH"
+echo "Install Battery Health Charging. Press enter to continue..."
+read -r
 
+echo "Install Vitals. Press enter to continue..."
+read -r
 
-echo "Installing Battery-Health-Charging..."
-gext install Battery-Health-Charging@maniacx.github.com
+echo "Install Dash to Dock. Press enter to continue..."
+read -r
 
+echo "Install Blur My Shell. Press enter to continue..."
+read -r
 
-echo "Installing Vitals..."
-gext install Vitals@CoreCoding.com
-
-
-echo "Installing Dash-to-Dock..."
-gext install dash-to-dock@micxgx.gmail.com
-
-
-echo "Installing blur-my-shell..."
-gext install blur-my-shell@aunetx
-
-
-echo "==============================================="
-echo "=== GNOME extensions installed successfully ==="
-echo "==============================================="
+echo "====================================="
+echo "=== GNOME configuration completed ==="
+echo "====================================="
